794409d248b5e0b38e34e9a963ba5324ac8d5125
Anastasia Zef <zeff1983@gmail.com>
2024-11-15 19:42:27 +0300

    Initial commit

---
+++ src/main/java/ru/netology/Radio.java
@@ -0,0 +1,54 @@
+package ru.netology;
+
+public class Radio {
+    public int currentChannel;
+
+    public void setToMinChannel() {
+        currentChannel = 0;
+    }
+
+    public void setToMaxChannel() {
+        currentChannel = 9;
+    }
+
+    public void nextChannel() {
+        if (currentChannel >= 0) {
+            currentChannel = currentChannel + 1;
+        }
+        if (currentChannel > 9) {
+            currentChannel = 0;
+        }
+        if (currentChannel < 0) {
+            currentChannel = 9;
+        }
+    }
+
+    public int currentVolume;
+
+    public void setToMinVolume() {
+        currentVolume = 0;
+    }
+
+    public void setToMaxVolume() {
+        currentVolume = 100;
+    }
+
+    public void ambitVolume() {
+        if (currentVolume < 0) {
+            setToMinVolume();
+        }
+        if (currentVolume > 100) {
+            setToMaxVolume();
+        }
+    }
+
+    public void increaseVolume() {
+
+        if (currentVolume >= 0) {
+            currentVolume = currentVolume + 1;
+        }
+
+    }
+
+
+}
---
+++ src/test/java/ru/netology/RadioTest.java
@@ -0,0 +1,148 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadioTest {
+
+    @Test
+    public void testCurrentChannel() {
+        Radio chan = new Radio();
+        chan.currentChannel = 5;
+
+        int expected = 5;
+        int actual = chan.currentChannel;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetToMinChannel() {
+        Radio chan = new Radio();
+        chan.setToMinChannel();
+
+        int expected = 0;
+        int actual = chan.currentChannel;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetToMaxChannel() {
+        Radio chan = new Radio();
+        chan.setToMaxChannel();
+
+        int expected = 9;
+        int actual = chan.currentChannel;
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testNextChannel() {
+        Radio chan = new Radio();
+        chan.currentChannel = 4;
+        chan.nextChannel();
+
+        int expected = 5;
+        int actual = chan.currentChannel;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotNextChannelAboveMax() {
+        Radio chan = new Radio();
+        chan.currentChannel = 10;
+        chan.nextChannel();
+
+        int expected = 0;
+        int actual = chan.currentChannel;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotNextChannelBelowMin() {
+        Radio chan = new Radio();
+        chan.currentChannel = -1;
+        chan.nextChannel();
+
+        int expected = 9;
+        int actual = chan.currentChannel;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testCurrentVolume() {
+        Radio vol = new Radio();
+        vol.currentVolume = 50;
+
+        int expected = 50;
+        int actual = vol.currentVolume;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetToMinVolume() {
+        Radio vol = new Radio();
+        vol.setToMinVolume();
+
+        int expected = 0;
+        int actual = vol.currentVolume;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSetToMaxVolume() {
+        Radio vol = new Radio();
+        vol.setToMaxVolume();
+
+        int expected = 100;
+        int actual = vol.currentVolume;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIncreaseVolume() {
+        Radio vol = new Radio();
+        vol.currentVolume = 54;
+        vol.increaseVolume();
+
+        int expected = 55;
+        int actual = vol.currentVolume;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void sholdNitAmbitVolumeLowMin() {
+        Radio vol = new Radio();
+        vol.currentVolume = -1;
+        vol.ambitVolume();
+
+        int expected = 0;
+        int actual = vol.currentVolume;
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void sholdNitAmbitVolumeAboveMax() {
+        Radio vol = new Radio();
+        vol.currentVolume = 100;
+        vol.ambitVolume();
+
+        int expected = 100;
+        int actual = vol.currentVolume;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
